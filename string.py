88dad5aa60b202613134d980c2571ce59475c8cf
Vijay khot <120132133+khvijay@users.noreply.github.com>
2024-02-18 18:48:48 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,19 @@
+name = "Vijay"
+friend = "Rohan"
+anotherFriend = "sadashiv"
+#print(name, anotherFriend, friend)
+
+apple = '''he said,
+hi vijay
+i want to eat an apple '''
+#print(apple)
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+# print(name[5]) # throws an error 
+print("lets use a for loop\n")
+for character in name:
+    print(character)
